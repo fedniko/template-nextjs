c5ea106dec743b2487482f65496515f94bb9026c
Hartaithan <hartaithan@gmail.com>
2021-07-07 11:48:00 +0900

    feat: add about-us page layout

--- templates/commerce/aboutUs/AboutAdvantages.tsx
+++ templates/commerce/aboutUs/AboutAdvantages.tsx
@@ -5,7 +5,42 @@ export default function AboutAdvantages() {
     <section className="aboutAdvantages">
       <Container>
         <Row>
-          <Col>ABOUT ADVANTAGES SECTION</Col>
+          <Col className="aboutAdvantages__col">
+            <h2 className="aboutAdvantages__col__title">
+              Makes Everything So Much Eastier
+              <br />
+              It’s Even More With Melor
+            </h2>
+            <p>
+              We believe that when we take care of our home, it takes care of
+              us. That’s why we make premium quality sheets, towels, robes, rugs
+              and all things soft and wonderful.
+            </p>
+            <div className="aboutAdvantages__col__imgs">
+              <img src="/commerce_img/aboutUs/img2.png" alt="img2" />
+              <img src="/commerce_img/aboutUs/img3.png" alt="img3" />
+            </div>
+            <p>
+              Sed vulputate elit vitae magna lacinia, vel bibendum neque
+              faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu,
+              pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus
+              et ipsum rutrum dignissim. Praesent ultrices posuere eros ac
+              tristique.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet isse potenti. Vesquam ante aliquet
+              lacusemper elit. Cras neque nulla, convallis non commodo et,
+              euismod nonsese. At vero eos et accusamus et iusto odio
+              dignissimos ducimus qui blanditiis praesentium.
+              <br />
+              <br />
+              Sed vulputate elit vitae magna lacinia, vel bibendum neque
+              faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu,
+              pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus
+              et ipsum rutrum dignissim. Praesent ultrices posuere eros ac
+              tristique.
+            </p>
+          </Col>
         </Row>
       </Container>
     </section>
