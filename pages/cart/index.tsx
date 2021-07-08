3257c5ba6559bdc656787b1918d25d889eb745cd
Hartaithan <hartaithan@gmail.com>
2021-07-08 10:47:17 +0900

    chore: rename Cart imports

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,16 +1,16 @@
 import Head from 'next/head';
 import MainLayout from '../../templates/commerce/MainLayout';
-import Cart from '../../templates/commerce/cart/Cart';
+import CartMain from '../../templates/commerce/cart/Cart';
 import ItemCarousel from '../../templates/commerce/itemList/ItemCarousel';
 
-export default function FAQHome() {
+export default function Cart() {
   return (
     <MainLayout>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Cart />
+      <CartMain />
       <ItemCarousel />
     </MainLayout>
   );
